24a6fc6c04f7c68928aa0c65466f29d6f06a3cfa
Yousaf Nabi <yousafn@gmail.com>
2022-04-24 21:07:58 +0100

    add newman-collection

---
+++ scripts/newmanCollection.js
@@ -0,0 +1,34 @@
+const { Collection, Item } = require("newman-collection");
+const newman = require("newman");
+
+let oCollection = new Collection([
+  // test GET
+  new Item("Test GET request")
+    .get("https://postman-echo.com/get?foo1=bar1&foo2=bar2")
+    .pm.test("This is test A", () => {
+      pm.response.to.be.ok;
+    })
+    .pm.test("This is test B", () => {
+      pm.response.to.be.ok;
+    }),
+  // test POST
+  new Item("Test POST request")
+    .post("https://postman-echo.com/post")
+    .headers({ "Content-Type": "text/plain" })
+    .body("test")
+    .pm.test("body should be same", () => {
+      pm.response.to.have.jsonBody("data", "test");
+    }),
+  // test auth
+  new Item("Test basic auth")
+    .get("https://postman-echo.com/basic-auth")
+    .auth.basic({ username: "postman", password: "password" })
+    .pm.test("Must be authenticated", () => {
+      pm.response.to.have.jsonBody("authenticated", true);
+    })
+]);
+
+newman.run({
+  collection: oCollection.collection,
+  reporters: ["cli"]
+});
